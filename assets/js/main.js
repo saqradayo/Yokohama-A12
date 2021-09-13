42d3efdd6a6938f183270f6ebca643591328e61a
saqradayo <89884023+saqradayo@users.noreply.github.com>
2021-09-13 10:31:56 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'A',
         },
         stage2: {
           q1: '2024',
@@ -26,7 +26,7 @@ const app = Vue.createApp({
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'プログラミング',
           // q2: 'かかか',
           // q3: 'ききき',
         }
